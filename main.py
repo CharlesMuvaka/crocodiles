27be624547c659deb69f3349358122e96ed32d33
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-12 05:16:07 +0300

    define a method to check for cycles in the graph

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def explore(graph, current_node, visited):
     for neighbor_node in graph.routes_dictionary[current_node]:
         explore(graph, neighbor_node, visited)
     return True
+
+
+def count_cycles(graph):  # creating a function to calculate the total number of cycles in the graph
+    visited = []
+    count = 0
+    for node in graph.routes_dictionary:
+        if explore(graph, node, visited):
+            count += 1
+    return count
