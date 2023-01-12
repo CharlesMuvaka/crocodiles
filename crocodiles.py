5d33b53e87d1367fc60a8c7723e90b3b9a3b7294
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-12 05:11:03 +0300

    add function to add a sighting to the frame.

--- crocodiles.py
+++ crocodiles.py
@@ -6,3 +6,12 @@ import sys
 # sorting the data in descending order
 def sort_data(data_frame, data_frame_column):
     return data_frame.sort_values(data_frame_column, ascending=False)
+
+
+def add_sighting(data_frame, location, water, update):
+    if not update:
+        data_frame.loc[len(data_frame)] = [len(data_frame), location.newX, location.newY, water, 1]
+    else:
+        data_frame.loc[
+            (data_frame['y'] == location[1]) & (data_frame['x'] == location[0]), ['number_of_sightings']] += 1
+    return data_frame
